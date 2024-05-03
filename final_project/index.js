d6ec67ffa44a3d828ed90aac1d14ef1fcea1279b
Jorge A. Reyes Jr. <10119138+jorgeareyesjr@users.noreply.github.com>
2024-05-03 00:02:11 -0700

    Add new customer authentication middleware to track a user's authentication status as the user navigates the app routes

--- final_project/index.js
+++ final_project/index.js
@@ -8,15 +8,38 @@ const app = express();
 
 app.use(express.json());
 
-app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
+app.use("/customer",
+	session({
+		secret: "fingerprint_customer",
+		resave: true,
+		saveUninitialized: true,
+	}),
+);
 
-app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+// Middleware which tells that the user is authenticated or not.
+app.use("/customer/auth/*", function auth(req, res, next) {
+	// Get the authorization object stored in the session.
+	if (req.session.authorization) {
+		// Get access token from session.
+		let token = req.session.authorization['accessToken'];
+
+		// Verify the token.
+		jwt.verify(token, "access-secret", (err, user) => {
+			if(!err){
+				req.user = user;
+				next();
+			} else {
+				return res.status(403).json({ message: "User is not authenticated." });
+			};
+		});
+	} else {
+		return res.status(403).json({ message: "User is not logged in." });
+	};
 });
- 
-const PORT =5000;
+
+const PORT = 5000;
 
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
 
-app.listen(PORT,()=>console.log("Server is running"));
+app.listen(PORT,() => console.log("Server is running"));
